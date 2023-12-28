2a69f7ba82e4827ac784d98530642c657e77bd6b
jsoch <jsochor013@gmail.com>
2023-12-28 16:28:29 +0100

    Add visual and some base classes

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,23 +1,21 @@
-﻿using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
+using SlidoCodingAssessment.Helpers;
 
 namespace SlidoCodingAssessment;
 
 /// <summary>
-/// Interaction logic for MainWindow.xaml
+///     Interaction logic for MainWindow.xaml
 /// </summary>
 public partial class MainWindow : Window
 {
     public MainWindow()
     {
         InitializeComponent();
+    }
+
+    protected override void OnSourceInitialized(EventArgs e)
+    {
+        IconHelper.RemoveIcon(this);
+        base.OnSourceInitialized(e);
     }
 }
