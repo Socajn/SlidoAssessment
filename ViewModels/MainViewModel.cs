ea08b976f12b8a3514a628f4058eea963e3b945e
jsoch <jsochor013@gmail.com>
2024-01-02 17:25:07 +0100

    - Update visual - Add Startup check - Prep. ViewModel

--- ViewModels/MainViewModel.cs
+++ ViewModels/MainViewModel.cs
@@ -4,16 +4,9 @@ namespace SlidoCodingAssessment.ViewModels;
 
 public class MainViewModel : ViewModelBase
 {
-    private string _firstName;
 
     public MainViewModel()
     {
-        FirstName = @"TestName";
-    }
-
-    public string FirstName
-    {
-        get => _firstName;
-        set => SetProperty(ref _firstName, value);
+        
     }
 }
